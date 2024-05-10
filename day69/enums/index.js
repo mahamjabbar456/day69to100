ae3223a84c287e9c5c545e3a64042c705e50b737
mahamjabbar456 <mahamjabbar320@gmail.com>
2024-05-10 09:06:45 +0500

    Add another example of enum

--- day69/enums/index.js
+++ day69/enums/index.js
@@ -10,4 +10,28 @@ var Season;
 })(Season || (Season = {}));
 let seasons = Season.spring;
 console.log(`Seaon is ${Season[seasons]}`);
+// Another example
+var Roles;
+(function (Roles) {
+    Roles["admin"] = "admin";
+    Roles["user"] = "user";
+})(Roles || (Roles = {}));
+let user1 = {
+    name: "John",
+    email: "john@gmail.com",
+    password: "29fjiew",
+    role: Roles.admin
+};
+let user2 = {
+    name: "Bob",
+    email: "bob@gmail.com",
+    password: "dienv45",
+    role: Roles.user
+};
+let isAdmin = (user) => {
+    let { name, role } = user;
+    return role === "admin" ? `${name} you allow to edit website` : `${name} you do not allow to edit website`;
+};
+console.log(isAdmin(user1));
+console.log(isAdmin(user2));
 export {};
--- day69/enums/index.ts
+++ day69/enums/index.ts
@@ -11,3 +11,38 @@ enum Season{
 
 let seasons:Season = Season.spring;
 console.log(`Seaon is ${Season[seasons]}`);
+
+// Another example
+enum Roles {
+    admin = "admin",
+    user = "user",
+}
+
+type LoginDetails = {
+    name:string,
+    email:string,
+    password:string,
+    role:Roles
+}
+
+let user1:LoginDetails = {
+    name :"John",
+    email:"john@gmail.com",
+    password:"29fjiew",
+    role:Roles.admin
+}
+
+let user2:LoginDetails = {
+    name :"Bob",
+    email:"bob@gmail.com",
+    password:"dienv45",
+    role:Roles.user
+}
+
+let isAdmin = (user:LoginDetails)=>{
+     let {name,role} = user;
+     return role ==="admin" ? `${name} you allow to edit website` : `${name} you do not allow to edit website`
+}
+
+console.log(isAdmin(user1));
+console.log(isAdmin(user2));
